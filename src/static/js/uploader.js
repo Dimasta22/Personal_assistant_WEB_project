800bc363fe7ead1d7a5d671381d3f851d6066ca4
Сергей Сытник <sytnikserhii22038@gmail.com>
2022-11-14 20:08:24 +0200

    some progress

---
+++ src/static/js/uploader.js
@@ -0,0 +1,17 @@
+(function() {
+
+  'use strict';
+
+  $('.input-file').each(function() {
+    var $input = $(this),
+        $label = $input.next('.js-labelFile'),
+        labelVal = $label.html();
+
+   $input.on('change', function(element) {
+      var fileName = '';
+      if (element.target.value) fileName = element.target.value.split('\\').pop();
+      fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass('has-file').html(labelVal);
+   });
+  });
+
+})();
