4a63186fd0431071479dc9d5e390e2139d7d7cf6
Rebutti <misha.ovsov@gmail.com>
2022-11-14 20:05:22 +0200

    created pages for contacts

--- src/models.py
+++ src/models.py
@@ -7,76 +7,83 @@ from datetime import datetime
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    tablename = "user"
     id = db.Column(db.Integer, primary_key=True)
     nick = db.Column(db.String(30), nullable=True, unique=True)
     hash = db.Column(db.String(255), nullable=True)
-    contacts = relationship('Contact', backref='users')
-    notes = relationship('Note', backref='users')
+    contacts = relationship("Contact", back_populates="user",
+                            cascade="all, delete",
+                            passive_deletes=True, )
 
 
 class Contact(db.Model):
-    __tablename__ = 'contacts'
+    tablename = "contact"
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=True)
     last_name = db.Column(db.String(30), nullable=True)
     birthday = db.Column(db.String(30), nullable=True)
-    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
-    user = relationship('User', cascade='all, delete', backref='contact')
-    emails = relationship('Email', backref='contacts')
-    addresses = relationship('Address', backref='contacts')
-    phones = relationship('Phone', backref='contacts')
+    user_id = db.Column(db.Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    user = relationship("User", back_populates="contacts")
+    emails = relationship("Email", back_populates="contact",
+                          cascade="all, delete",
+                          passive_deletes=True, )
+    addresses = relationship("Address", back_populates="contact",
+                             cascade="all, delete",
+                             passive_deletes=True, )
+    phones = relationship("Phone", back_populates="contact",
+                          cascade="all, delete",
+                          passive_deletes=True, )
 
 
 class Email(db.Model):
-    __tablename__ = 'emails'
+    tablename = "emails"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), nullable=True)
-    contact_id = db.Column(db.Integer, ForeignKey('contacts.id'), nullable=False)
-    contact = relationship('Contact', cascade='all, delete', backref='email')
+    contact_id = db.Column(db.Integer, ForeignKey("contact.id", ondelete="CASCADE"), nullable=False)
+    contact = relationship("Contact", back_populates="emails")
 
 
 class Address(db.Model):
-    __tablename__ = 'addresses'
+    tablename = "addresses"
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(250), nullable=True)
-    contact_id = db.Column(db.Integer, ForeignKey('contacts.id'), nullable=False)
-    contact = relationship('Contact', cascade='all, delete', backref='address')
+    contact_id = db.Column(db.Integer, ForeignKey("contact.id", ondelete="CASCADE"), nullable=False)
+    contact = relationship("Contact", back_populates="addresses")
 
 
 class Phone(db.Model):
-    __tablename__ = 'phones'
+    tablename = "phones"
     id = db.Column(db.Integer, primary_key=True)
     phone = db.Column(db.String(20), nullable=True)
-    contact_id = db.Column(db.Integer, ForeignKey('contacts.id'), nullable=False)
-    contact = relationship('Contact', cascade='all, delete', backref='phone')
+    contact_id = db.Column(db.Integer, ForeignKey("contact.id", ondelete="CASCADE"), nullable=False)
+    contact = relationship("Contact", back_populates="phones")
 
 
-note_m2m_tag = db.Table(
-    "note_m2m_tag",
-    db.Model.metadata,
-    db.Column("id", db.Integer, primary_key=True),
-    db.Column("note", db.Integer, ForeignKey("notes.id")),
-    db.Column("tag", db.Integer, ForeignKey("tags.id")),
-)
+# note_m2m_tag = db.Table(
+#     "note_m2m_tag",
+#     db.Model.metadata,
+#     db.Column("id", db.Integer, primary_key=True),
+#     db.Column("note", db.Integer, ForeignKey("notes.id")),
+#     db.Column("tag", db.Integer, ForeignKey("tags.id")),
+# )
 
 
-class Note(db.Model):
-    __tablename__ = "notes"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    created = db.Column(db.DateTime, default=datetime.now())
-    description = db.Column(db.String(150), nullable=False)
-    done = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
-    user = relationship('User', cascade='all, delete', backref='note')
-    tags = relationship("Tag", secondary=note_m2m_tag, backref="notes")
+# class Note(db.Model):
+#     __tablename__ = "notes"
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(50), nullable=False)
+#     created = db.Column(db.DateTime, default=datetime.now())
+#     description = db.Column(db.String(150), nullable=False)
+#     done = db.Column(db.Boolean, default=False)
+#     user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+#     user = relationship('User', cascade='all, delete', backref='note')
+#     tags = relationship("Tag", secondary=note_m2m_tag, backref="notes")
 
 
-class Tag(db.Model):
-    __tablename__ = "tags"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(25), nullable=False, unique=True)
+# class Tag(db.Model):
+#     __tablename__ = "tags"
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(25), nullable=False, unique=True)
 
-    def __repr__(self) -> str:
-        return self.name
+#     def __repr__(self) -> str:
+#         return self.name
